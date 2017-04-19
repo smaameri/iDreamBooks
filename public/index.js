9b45d6a71531be1bc1ba4d499c8759f9c7a2da9a
Sami El Maameri <sami@Samis-MacBook-Air.local>
2017-04-19 18:05:04 +0400

    ready

--- public/index.js
+++ public/index.js
@@ -73,7 +73,7 @@ $(() => {
 	
 	//Load inital view based on url hash value
   var hashURL = decodeURI(window.location.hash);
-	renderPage(hashURL);
+	//renderPage(hashURL);
 	
 	// On every hash change the render function is called with the new hash.
 	// This is how the navigation of our app happens.
