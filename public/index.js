06c9b78211d5e7fb92a1d78a4c8d3d121d4b5d20
Sami El Maameri <sami@Samis-MacBook-Air.local>
2017-04-19 17:59:15 +0400

    ready

--- public/index.js
+++ public/index.js
@@ -55,9 +55,9 @@ $(() => {
 	$.getScript( "templates/shows_list.js");
 	$.getScript( "templates/book_details.js", () => {
 		templates.bookDetails = bookDetailsTemplate;
-		var url 	= decodeURI(window.location.hash);
-		var temp = url.split('/')[0];
-		if(temp = '#bookdetails')
+		var hash 	= decodeURI(window.location.hash);
+		var temp  = hash.split('/')[0];
+		if(temp   = '#bookdetails')
 			renderPage();
 	});
 
